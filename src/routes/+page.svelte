6c08ae4f797782f7b45185d401308ad03c9862fd
Robertx999 <robert.e4200@gmail.com>
2023-03-30 18:22:36 +0200

    Justified text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,8 +32,8 @@
 	jak również zmieniające się style artystyczne i wpływy kulturowe, które wpłynęły na tę dziedzinę.<br
 	/><br />
 
-	Najwcześniejsze przykłady architektury sięgają starożytnych cywilizacji Mezopotamii, Egiptu i
-	Grecji, gdzie architekci i budowniczowie tworzyli monumentalne budowle, takie jak świątynie,
+	Najwcześniejsze przykłady architektury sięgają starożytnych cywilizacji Mezopotamii, Egiptu
+	i Grecji, gdzie architekci i budowniczowie tworzyli monumentalne budowle, takie jak świątynie,
 	pałace i grobowce, które do dziś budzą podziw. Stąd style architektoniczne ewoluowały przez wieki,
 	od ozdobnych gotyckich katedr w średniowieczu po wielkie barokowe pałace w XVII i XVIII wieku.<br
 	/><br />
@@ -146,46 +146,46 @@
 	Architektura wczesnochrześcijańska pojawiła się w późnym Cesarstwie Rzymskim, w IV wieku n.e., w
 	wyniku rozprzestrzeniania się chrześcijaństwa. Jednym z najwcześniejszych przykładów tego stylu
 	jest stara Bazylika św. Piotra w Rzymie, zbudowana przez cesarza Konstantyna na początku IV wieku.
-	Głównymi cechami tej architektury były prostota, trzeźwość i funkcjonalność. Budowle były
-	zazwyczaj bazylikami z prostokątną nawą i nawami bocznymi. Miały drewniane dachy i często były
-	zdobione mozaikami i freskami.
+	Głównymi cechami tej architektury były prostota, trzeźwość i funkcjonalność. Budowle były
+	zazwyczaj bazylikami z prostokątną nawą i nawami bocznymi. Miały drewniane dachy i często były
+	zdobione mozaikami i freskami.
 	<br /><br />
 	Elementy architektoniczne wczesnochrześcijańskich budowli były pod wpływem rzymskich precedensów. Jedną
 	z najważniejszych cech tych budynków było zastosowanie łuku, który pozwalał na uzyskanie większych
 	przestrzeni wewnętrznych. Łuki były często wykorzystywane do tworzenia arkad, które zapewniały efekt
-	dekoracyjny i pomagały wspierać konstrukcję.
+	dekoracyjny i pomagały wspierać konstrukcję.
 	<br /><br />
 	Wczesna architektura chrześcijańska wykorzystywała również motyw krzyża jako symbolu chrześcijaństwa.
-	Wiele kościołów było budowanych w kształcie krzyża, z centralną nawą i czterema ramionami. Ołtarz znajdował
+	Wiele kościołów było budowanych w kształcie krzyża, z centralną nawą i czterema ramionami. Ołtarz znajdował
 	się zwykle na przecięciu ramion.
 	<br /><br />
 	Oprócz kościołów architektura wczesnochrześcijańska obejmowała również inne budynki, takie jak baptysteria,
-	mauzolea i bazyliki. Baptysteria były małymi budynkami używanymi do chrztów, podczas gdy mauzolea służyły
-	do pochówków. Bazyliki służyły do organizowania publicznych zgromadzeń, takich jak sądy i targi.
+	mauzolea i bazyliki. Baptysteria były małymi budynkami używanymi do chrztów, podczas gdy mauzolea służyły
+	do pochówków. Bazyliki służyły do organizowania publicznych zgromadzeń, takich jak sądy i targi.
 </p>
 
 <SectionHeader src={byzantine}>
 	<div class="backdrop-brightness-[.65] contrast-[4] text-white w-fit py-5 px-10">Bizancjum</div>
 </SectionHeader>
 <p class="py-8 px-[20%] first-letter:text-7xl first-letter:tracking-widest">
-	Architektura bizantyjska powstała we wschodniej części Imperium Rzymskiego i była pod silnym
+	Architektura bizantyjska powstała we wschodniej części Imperium Rzymskiego i była pod silnym
 	wpływem chrześcijaństwa. Najbardziej znanym przykładem architektury bizantyjskiej jest Hagia
 	Sophia, która została zbudowana w Konstantynopolu (obecnie Stambuł) w VI wieku. Kopuła budynku
-	była cudem technologicznym w tym czasie i nadal jest uważana za jeden z największych wyczynów
+	była cudem technologicznym w tym czasie i nadal jest uważana za jeden z największych wyczynów
 	inżynieryjnych w historii. <br /><br />Architektura bizantyjska charakteryzuje się wykorzystaniem
-	misternych mozaik, które pokrywały ściany i sufity kościołów i innych budynków religijnych.
-	Mozaiki przedstawiały sceny z Biblii i żywoty świętych, a ich zadaniem było inspirowanie wiernych
+	misternych mozaik, które pokrywały ściany i sufity kościołów i innych budynków religijnych.
+	Mozaiki przedstawiały sceny z Biblii i żywoty świętych, a ich zadaniem było inspirowanie wiernych
 	i wzmacnianie nauk kościoła.<br /><br />
 	Inną kluczową cechą architektury bizantyjskiej jest użycie kopuł, które często były używane do pokrycia
-	centralnej przestrzeni kościoła. Kopuły te były zwykle ozdobione mozaikami i były wspierane przez duże
-	łuki i filary. <br /><br />Użycie bogato zdobionego marmuru i kamienia było również znakiem
+	centralnej przestrzeni kościoła. Kopuły te były zwykle ozdobione mozaikami i były wspierane przez duże
+	łuki i filary. <br /><br />Użycie bogato zdobionego marmuru i kamienia było również znakiem
 	rozpoznawczym architektury bizantyjskiej. Ściany wielu kościołów były pokryte marmurowymi płytami,
-	a skomplikowane rzeźby i płaskorzeźby zdobiły drzwi i okna. <br /><br />Bizantyjscy architekci
-	znani byli również z zastosowania symetrii i proporcji. Budynki często miały kształt krzyża, z
-	centralną kopułą i czterema ramionami. Projekt ten miał symbolizować ukrzyżowanie Jezusa i
+	a skomplikowane rzeźby i płaskorzeźby zdobiły drzwi i okna. <br /><br />Bizantyjscy architekci
+	znani byli również z zastosowania symetrii i proporcji. Budynki często miały kształt krzyża, z
+	centralną kopułą i czterema ramionami. Projekt ten miał symbolizować ukrzyżowanie Jezusa i
 	podkreślać znaczenie kościoła w życiu wiernych. <br /><br />Architektura bizantyjska była
-	odzwierciedleniem głębokich tradycji religijnych i kulturowych Cesarstwa Wschodniorzymskiego. Jej
-	misterne mozaiki, strzeliste kopuły i bogato zdobione powierzchnie do dziś wzbudzają zachwyt i
+	odzwierciedleniem głębokich tradycji religijnych i kulturowych Cesarstwa Wschodniorzymskiego. Jej
+	misterne mozaiki, strzeliste kopuły i bogato zdobione powierzchnie do dziś wzbudzają zachwyt i
 	podziw.
 </p>
 
@@ -195,26 +195,26 @@
 	</div>
 </SectionHeader>
 <p class="py-8 px-[20%] first-letter:text-7xl first-letter:tracking-widest">
-	Romanizm to styl, który pojawił się w Europie w X wieku i trwał do połowy XII wieku.
-	Charakteryzował się on powrotem do rzymskich form i technik architektonicznych, a także włączeniem
-	elementów bizantyjskich i islamskich. Okres ten widział budowę wielu kościołów i klasztorów, które
-	zostały zbudowane z silnym naciskiem na stabilność i wytrzymałość. Jedną z najbardziej
+	Romanizm to styl, który pojawił się w Europie w X wieku i trwał do połowy XII wieku.
+	Charakteryzował się on powrotem do rzymskich form i technik architektonicznych, a także włączeniem
+	elementów bizantyjskich i islamskich. Okres ten widział budowę wielu kościołów i klasztorów, które
+	zostały zbudowane z silnym naciskiem na stabilność i wytrzymałość. Jedną z najbardziej
 	charakterystycznych cech architektury romańskiej było użycie okrągłych łuków, które często były
-	ozdobione ornamentami i rzeźbami.
+	ozdobione ornamentami i rzeźbami.
 	<br /><br />
-	Budynki romańskie miały również zazwyczaj grube ściany, małe okna i beczkowate sufity, co dawało poczucie
-	solidności i siły. Ponadto wiele kościołów z tego okresu miało transept, czyli krzyżowy plan podłogi,
+	Budynki romańskie miały również zazwyczaj grube ściany, małe okna i beczkowate sufity, co dawało poczucie
+	solidności i siły. Ponadto wiele kościołów z tego okresu miało transept, czyli krzyżowy plan podłogi,
 	który pozwalał na większy przepływ ludzi podczas ceremonii religijnych. Fasady romańskich budynków
 	były często zdobione ozdobnymi kamiennymi rzeźbami, przedstawiającymi sceny z Biblii lub żywoty świętych.
 	<br /> <img src={romanesque_style} alt="rekonstrukcja wyglądu romańskiej katedry w Konstancji" />
 	<br />
 	Innym godnym uwagi aspektem architektury romańskiej było użycie wież, które często były budowane na
-	rogach budynków lub jako oddzielne konstrukcje. Wieże te służyły zarówno celom praktycznym, jak i symbolicznym,
-	zapewniając punkt obserwacyjny dla obrony budynku, a także symbolizując siłę i władzę kościoła.
+	rogach budynków lub jako oddzielne konstrukcje. Wieże te służyły zarówno celom praktycznym, jak i symbolicznym,
+	zapewniając punkt obserwacyjny dla obrony budynku, a także symbolizując siłę i władzę kościoła.
 	<br /><br />
-	Architektura romańska charakteryzowała się przewagą funkcji nad formą, z naciskiem na solidność i trwałość.
+	Architektura romańska charakteryzowała się przewagą funkcji nad formą, z naciskiem na solidność i trwałość.
 	Chociaż styl ten został ostatecznie zastąpiony przez gotyk, wiele przykładów architektury romańskiej
-	można nadal znaleźć w całej Europie i są one obchodzone ze względu na ich wyjątkową estetykę i znaczenie
+	można nadal znaleźć w całej Europie i są one obchodzone ze względu na ich wyjątkową estetykę i znaczenie
 	historyczne.
 </p>
 
@@ -224,24 +224,24 @@
 	</div>
 </SectionHeader>
 <p class="py-8 px-[20%] first-letter:text-7xl first-letter:tracking-widest">
-	Architektura gotycka pojawiła się w XII wieku i trwała do XVI wieku. Charakteryzuje się
-	spiczastymi łukami, żebrowymi sklepieniami i latającymi przyporami. Celem architektury gotyckiej
+	Architektura gotycka pojawiła się w XII wieku i trwała do XVI wieku. Charakteryzuje się
+	spiczastymi łukami, żebrowymi sklepieniami i latającymi przyporami. Celem architektury gotyckiej
 	było stworzenie wielkich, strzelistych przestrzeni wypełnionych światłem. Łuk spiczasty umożliwiał
-	budowniczym wznoszenie wyższych i bardziej skomplikowanych budynków, ponieważ był w stanie
+	budowniczym wznoszenie wyższych i bardziej skomplikowanych budynków, ponieważ był w stanie
 	utrzymać większy ciężar niż łuk okrągły. Z kolei sklepienie żebrowe pozwalało na uzyskanie jeszcze
-	większej wysokości i większych okien.
+	większej wysokości i większych okien.
 	<br /><br />
 	Architektura gotycka charakteryzowała się również wyszukanymi dekoracjami, takimi jak misterne kamienne
-	trakcje na oknach i drzwiach oraz ozdobne rzeźby i płaskorzeźby. Dekoracje te często zawierały motywy
-	religijne, takie jak święci i anioły. Gotyckie katedry miały wzbudzać respekt i dawać poczucie boskości.
-	W związku z tym często zawierały okazałe elementy, takie jak strzeliste wieże, masywne witraże i misterne
+	trakcje na oknach i drzwiach oraz ozdobne rzeźby i płaskorzeźby. Dekoracje te często zawierały motywy
+	religijne, takie jak święci i anioły. Gotyckie katedry miały wzbudzać respekt i dawać poczucie boskości.
+	W związku z tym często zawierały okazałe elementy, takie jak strzeliste wieże, masywne witraże i misterne
 	rzeźby.
 	<br /><br />
 	Jednym z najbardziej znanych przykładów architektury gotyckiej jest Katedra Notre Dame w Paryżu. Jej
-	budowę rozpoczęto w 1163 roku, a zakończono w 1345 roku. Strzelista nawa katedry i witraże są świadectwem
-	wielkości i piękna architektury gotyckiej. Innym godnym uwagi przykładem jest Katedra w Canterbury
-	w Anglii, która została zbudowana w XI wieku i przeszła znaczną rozbudowę i renowację w stylu gotyckim
-	w XII i XIII wieku. Jej wspaniałe krużganki i sklepienia wachlarzowe są znanymi przykładami gotyckiego
+	budowę rozpoczęto w 1163 roku, a zakończono w 1345 roku. Strzelista nawa katedry i witraże są świadectwem
+	wielkości i piękna architektury gotyckiej. Innym godnym uwagi przykładem jest Katedra w Canterbury
+	w Anglii, która została zbudowana w XI wieku i przeszła znaczną rozbudowę i renowację w stylu gotyckim
+	w XII i XIII wieku. Jej wspaniałe krużganki i sklepienia wachlarzowe są znanymi przykładami gotyckiego
 	wzornictwa.
 </p>
 
@@ -307,8 +307,8 @@
 	Występowała ona od końca XVII wieku do początku XIX wieku. Charakterystycznym elementem budowli
 	tego okresu jest portyk czyli otwarty ganek kolumnowy poprzedzający wejście. Budynki wyróżniały
 	się monumentalnym stylem z oszczędną ilością dekoracji. Jako ozdoby wykorzystywano girlandy, wazy,
-	rozety lub liście kantu. Stawiano je najczęściej na planie koła lub prostokąta i dbano, by były
-	zwarte i symetryczne. Wnętrza były dobrze oświetlone, a sufity pokryte polichromią lub kasetonami.
+	rozety lub liście kantu. Stawiano je najczęściej na planie koła lub prostokąta i dbano, by były
+	zwarte i symetryczne. Wnętrza były dobrze oświetlone, a sufity pokryte polichromią lub kasetonami.
 	W stylu klasycystycznym budowano liczne obiekty użyteczności publicznej. Pałace były niskie, z
 	wyraźnie zaakcentowaną osią symetrii. Jednym z najwybitniejszych architektów tego okresu był Jean
 	François Chalgrin, który może być znany z Łuku Triumfalnego.
@@ -316,11 +316,13 @@
 
 <div class="h-1 w-full mt-72 rounded-[100%] bg-black" />
 
-<div class="pt-72 pb-10 px-[20%]">
+<div class="h-screen flex flex-col justify-end pb-10 px-[20%]">
 	<h3>
-		<div class="leading-[7rem]">
+		<div class="leading-[5rem]">
 			<h2 class="w-fit bg-black text-white after:content-none">Wykonanie:</h2>
 			Szymon M., Robert P.<br />
+			<h2 class="w-fit bg-black text-white after:content-none">Kod źródłowy strony:</h2>
+			<a href="https://github.com/Robertx999/architecture-history-presentation">Github.com</a><br />
 			<h2 class="w-fit bg-black text-white after:content-none">Źródła:</h2>
 		</div>
 		<div>
